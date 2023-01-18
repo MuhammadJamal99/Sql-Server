2234827c250667f095817c0c7621fefa316a38a9
MuhammadJamal99 <muhammadjammi18@gmail.com>
2023-01-18 03:55:01 +0200

    Sql Server Course Day 1

--- FirstDay1.sql
+++ FirstDay1.sql
@@ -2,4 +2,8 @@
 
 --ALTER Database Sample2 Modify Name = Sample3;
 
-sp_renameDB 'Sample3', 'Sample4';
+--sp_renameDB 'Sample3', 'Sample4';
+
+--DROP DATABASE Sample2;
+
+--ALTER DATABASE Sample2 SET SINGLE_USER With Rollback Immediate;
